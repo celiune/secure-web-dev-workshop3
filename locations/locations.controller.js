46082242253dfe846ba2db1ef3708f2d3e16b0cc
celiune <celineliu1811@gmail.com>
2022-12-01 20:08:39 +0100

    Create: /locations: Post method and addLocations method

--- locations/locations.controller.js
+++ locations/locations.controller.js
@@ -4,9 +4,22 @@
 const router = require('express').Router()
 const locationsService = require('./locations.service')
 
-router.get('/locations', (req, res) => {
-	return res.status(200).send({locations: []})
+// Retrieve data of all locations
+router.get('/locations', async (req, res) => {
+	//return res.status(200).send({locations: []})
+	const allLocations = await locationsService.findAll()
+	res.json(allLocations)
 })
 
+// Create routes at Presentation Layer = use POST method route
+router.post('/locations', async (req, res) => {
+	try{
+		const locations = await locationsService.addLocation({...req.body})
+		return res.status(200).send(locations)
+	}catch (e){
+		if (e.message === "Missing film name") return res.status(400).send("The film name is not here, how am I supposed to add this")
+        return res.status(400).send("Bad Request, Try again !")
+	}
+  });
 
 module.exports = router
--- locations/locations.service.js
+++ locations/locations.service.js
@@ -2,8 +2,29 @@
 
 const Location = require('./locations.model')
 
-function findAll () {
-	return [1,2,3,4]
+async function findAll () {
+	const locations = await Location.find()
+    if (!locations) throw new Error("Locations not found")
+    return locations
 }
 
+async function findOne(id){
+	const location = await Location.findById(id)
+	if (!location) throw new Error("Location not found")
+	return location
+}
+
+async function addLocation(data){
+	try{
+		const instance = new Location(data)
+		return instance.save()
+	}catch(e){
+		throw new Error("Missing film name")
+	}
+}
+
+
 module.exports.findAll = findAll
+module.exports.findOne = findOne
+module.exports.addLocation = addLocation
+
